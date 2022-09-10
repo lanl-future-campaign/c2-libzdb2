88aa24e023a3f59c75890b971b718f5167246a0e
Qing Zheng <qzheng@lanl.gov>
2022-09-10 14:17:43 -0600

    reformat zdb output to files

--- src/libzdb_pp.cc
+++ src/libzdb_pp.cc
@@ -1,17 +1,40 @@
+#include <errno.h>
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <string>
 #include <vector>
 
-static std::vector<std::string> map;
-
+/*
+ * Example. ./zdb_pp [dev#] [pool_name]
+ */
 int
 main(int argc, char *argv[])
 {
-	map.resize(5);
+	const char *poolname = "pool";
+	int n = 5;
+	if (argc > 1) {
+		n = atoi(argv[1]);
+		if (n <= 0) {
+			fprintf(stderr, "Invalid dev count: '%s'\n", argv[1]);
+			return -1;
+		}
+	}
+	if (argc > 2) {
+		poolname = argv[2];
+	}
+	std::vector<FILE *> map;
+	map.resize(n);
+	char tmp[100];
 	for (int i = 0; i < map.size(); i++) {
-		map[i].reserve(4096);
+		snprintf(tmp, sizeof(tmp), "%s-%d", poolname, i);
+		map[i] = fopen(tmp, "w");
+		if (!map[i]) {
+			fprintf(stderr, "Cannot open file %s: %s\n", tmp,
+			    strerror(errno));
+			return -1;
+		}
 	}
 	std::string line;
 	while (std::cin >> line) {
@@ -22,11 +45,12 @@ main(int argc, char *argv[])
 		int devidx = atoi(line.substr(1, d1 - 1).c_str());
 		std::string::size_type d2 = line.find_first_of(',', d1 + 1);
 		std::string offset = line.substr(d1 + 1, d2 - d1 - 1);
-		map[devidx].append(offset);
-		map[devidx].append(",");
+		fputs(offset.c_str(), map[devidx]);
+		fputc('\n', map[devidx]);
 	}
 	for (int i = 0; i < map.size(); i++) {
-		std::cout << i << ":" << map[i].c_str() << std::endl;
+		fflush(map[i]);
+		fclose(map[i]);
 	}
 	return 0;
 }
